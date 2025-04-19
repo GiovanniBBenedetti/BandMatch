45fbdda577cad62489189bbbc26a9d7d6e8c76b3
Davi Leocadio <davi.leocadio.sen@gmail.com>
2025-04-19 03:03:51 -0300

    Var de cores add

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -8,12 +8,12 @@ export default function Header() {
             <nav className="navbar navbar-expand-lg rounded-bottom-5">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        <Image 
-                        src="/logo_v.png"
-                        alt=""
-                        width={100}
-                        height={100}
-                        className="img-fluid igr"
+                        <Image
+                            src="/logo_v.png"
+                            alt=""
+                            width={100}
+                            height={100}
+                            className="img-fluid"
                         />
                     </a>
                     <button
@@ -30,29 +30,37 @@ export default function Header() {
                     <div className="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul className="navbar-nav d-flex align-items-center justify-content-center w-100 gap-5">
                             <li className="nav-item">
-                                <a className="nav-link active text-danger fw-bold" aria-current="page" href="#">
+                                <a className="nav-link active fw-bold text-nav" aria-current="page" href="#">
                                     Home
                                 </a>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link active text-danger fw-bold" aria-current="page" href="#">
+                                <a className="nav-link active fw-bold text-nav" aria-current="page" href="#">
                                     Gêneros
                                 </a>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link active text-danger fw-bold" aria-current="page" href="#">
+                                <a className="nav-link active fw-bold text-nav" aria-current="page" href="#">
                                     Contato
                                 </a>
                             </li>
 
 
                         </ul>
-                        <div className="">
-                            <input type="text" name="" className="rounded-4 bg-light text-danger border-danger" />
-                            <i class="bi bi-search"></i>
+                        <div className="mb-3">
+                            <label className="">
+                                Multiple files input example
+                            </label>
+                            <input
+                                className="form-control"
+                                type="file"
+                                id="formFileMultiple"
+                                multiple=""
+                            />
                         </div>
+
                     </div>
                 </div>
             </nav>
