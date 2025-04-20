8ea0670673ebe6827c14dd2929a82d48624c4a9b
GiovanniBBenedetti <senaigiovannibenedetti@gmail.com>
2025-04-20 12:51:25 -0300

    Imagem principal adicionada

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import './home.css'
+
+export default function Home() {
+  return (
+  <>
+
+<img src="https://placehold.co/1600x500" className="d-block w-100" alt="..." />
+  </>
+  );
+}
