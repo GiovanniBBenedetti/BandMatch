30dfbd6334230420e36932149a75a934e9202578
GiovanniBBenedetti <senaigiovannibenedetti@gmail.com>
2025-04-22 22:29:10 -0300

    Campo de busca alterado

--- src/Components/busca/page.jsx
+++ src/Components/busca/page.jsx
@@ -16,20 +16,20 @@ export default function Busca() {
   };
 
   return (
-   
-      <form onSubmit={handleSearch} className="d-flex" role="search">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Nome, estilo Musical, instrumento ... "
-          aria-label="Nome, estilo Musical, instrumento ... "
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button className="btn" type="submit">
-          Search
-        </button>
-      </form>
+
+    <form className="container-input" onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Search"
+        name="text"
+        className="input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <svg fill="#000000" width="20px" height="20px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+        <path d="M790.588 1468.235c-373.722 0-677.647-303.924-677.647-677.647 0-373.722 303.925-677.647 677.647-677.647 373.723 0 677.647 303.925 677.647 677.647 0 373.723-303.924 677.647-677.647 677.647Zm596.781-160.715c120.396-138.692 193.807-319.285 193.807-516.932C1581.176 354.748 1226.428 0 790.588 0S0 354.748 0 790.588s354.748 790.588 790.588 790.588c197.647 0 378.24-73.411 516.932-193.807l516.028 516.142 79.963-79.963-516.142-516.028Z" fillRule="evenodd" />
+      </svg>
+    </form>
 
   );
 }
