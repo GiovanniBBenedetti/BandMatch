9278676af1547418576f6593aa0bcc1a24f91126
GiovanniBBenedetti <senaigiovannibenedetti@gmail.com>
2025-04-22 22:32:40 -0300

    Banner de fundo adicionado

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,13 @@ import './home.css'
 export default function Home() {
   return (
   <>
+  <Link href='/users'>
+  <div className="home-background">
+      <img src="./fundoHome.png" className="d-block w-100" alt="..." />
+    </div>
+  </Link>
 
-<img src="https://placehold.co/1600x500" className="d-block w-100" alt="..." />
+
   </>
   );
 }
