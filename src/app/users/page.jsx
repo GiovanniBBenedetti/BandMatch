0d202a3c599b5842b2addf197770328eafd99ae7
GiovanniBBenedetti <senaigiovannibenedetti@gmail.com>
2025-04-22 22:30:02 -0300

    Campo Filtro Funcionando

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,26 +1,17 @@
 'use client'
+
 import { useState, useEffect } from 'react'
 import './users.css'
 
 export default function Users() {
   const [usuarios, setUsuario] = useState([])
-  const [number, setNumber] = useState(9)
-
-  function resposta() {
-    const novoNumero = number + 9
-    setNumber(novoNumero)
-
-    fetch(`http://localhost:3333/users?_limit=${novoNumero}`)
-      .then((response) => response.json())
-      .then((information) => setUsuario(information))
-  }
-
-  useEffect(() => {
-    fetch("http://localhost:3333/users?_limit=9")
-      .then((response) => response.json())
-      .then((information) => setUsuario(information))
-  }, [])
-
+  const [idade, setIdade] = useState(18)
+  const [usuariosFiltrados, setFiltragem] = useState([])
+  const [instrumentosSelecionados, setInstrumentosSelecionados] = useState([])
+  const [estilosSelecionados, setEstilosSelecionados] = useState([])
+  const [generoSelecionado, setGeneroSelecionado] = useState([])
+  const instrumentosCheckBox = [ "guitarra", "baixo", "bateria", "vocal", "piano", "teclado", "saxofone", "controladora"]
+  const estilosMusicaisCheckbox = ["rock", "pop", "raggae", "jazz", "funk", "eletronica"]
   const instrumentoImagens = {
     guitarra: "/guitarra.png",
     bateria: "/bateria.png",
@@ -32,87 +23,158 @@ export default function Users() {
     vocal: "/vocal.png",
   }
 
+  const handleCheckbox = (e, setter) => {
+    const { value, checked } = e.target
+    setter(prev =>
+      checked ? [...prev, value] : prev.filter(item => item !== value)
+    )
+  }
+
+  const filtragem = () => {
+    const filtrados = usuarios.filter((user) => {
+      const atendeIdade = user.idade >= idade
+
+      const atendeInstrumentos =
+        instrumentosSelecionados.length === 0 ||
+        instrumentosSelecionados.every(instr => user.instrumentos.includes(instr))
+
+      const atendeEstilos =
+        estilosSelecionados.length === 0 ||
+        estilosSelecionados.every(estilo => user.estilos_musicais.includes(estilo))
+
+        const atendeGenero =
+        generoSelecionado.length === 0 ||
+        generoSelecionado.every(genero => user.genero.includes(genero))  
+
+      return atendeIdade && atendeInstrumentos && atendeEstilos && atendeGenero
+    })
+
+    console.log(filtrados)
+    setFiltragem(filtrados)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3333/users")
+      .then((response) => response.json())
+      .then((information) => {
+        setUsuario(information)
+        setFiltragem(information)
+      })
+  }, [])
+
+
   return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-3">
-            <div className="filtros p-3 bg-white shadow-sm rounded">
-              <h5>Filtros</h5>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="filtros p-3 bg-white shadow rounded">
+            <h5>Categorias relacionadas</h5>
 
-        
-              <div className="mb-3">
-                <label>Idade</label>
-                <div className="d-flex align-items-center gap-2">
-                  <input
-                    type="range"
-                    placeholder="Mín"
-                    className="form-control"
-                    style={{ width: "150px" }}
-                  />
-       
-                </div>
+            <div className="mb-3">
+              <label>Idade</label>
+              <div className="d-flex align-items-center gap-2">
+                <input
+                  type="range"
+                  min="18"
+                  max="90"
+                  value={idade}
+                  onChange={(e) => setIdade(Number(e.target.value))}
+                  className="form-control"
+                  style={{ width: "150px" }}
+                />
               </div>
-
-        
-              <div className="mb-3">
-                <label>Gêneros Musicais</label>
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="rock" />
-                  <label className="form-check-label" htmlFor="rock">Rock</label>
-                </div>
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="pop" />
-                  <label className="form-check-label" htmlFor="pop">Pop</label>
-                </div>
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="mpb" />
-                  <label className="form-check-label" htmlFor="mpb">Ragae</label>
-
-                </div>
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="mpb" />
-                  <label className="form-check-label" htmlFor="mpb">Jazz</label>
-                </div>
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="mpb" />
-                  <label className="form-check-label" htmlFor="mpb">Funk</label>
-                </div>
-                <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="mpb" />
-                  <label className="form-check-label" htmlFor="mpb">Eletrônica</label>
-                </div>
-              </div>
-
-           
-              <div className="mb-3">
-                <label>Instrumentos</label>
-                {Object.keys(instrumentoImagens).map((inst, i) => (
-                  <div className="form-check" key={i}>
-                    <input type="checkbox" className="form-check-input" id={inst} />
-                    <label className="form-check-label" htmlFor={inst}>
-                      {inst.charAt(0).toUpperCase() + inst.slice(1)}
-                    </label>
-                  </div>
-                ))}
-              </div>
-
-              <button className="btn btn-primary w-100">Filtrar</button>
+              <span>{idade} anos</span>
             </div>
-          </div>
 
-        
-          <div className="col-md-9">
-            <div className='container mb-5'>
-              <div className="row">
-                {usuarios.map(cards => (
+
+
+       <div className="mb-3">
+              <label>Genêro</label>
+         
+                <div  className="form-check d-flex align-items-center gap-2 p-0">
+                  <input
+                    type="checkbox"
+                    className="ui-checkbox"
+                    value="Masculino"
+                    onChange={(e) => handleCheckbox(e, setGeneroSelecionado)}
+                  />
+                  <label className="form-check-label">
+                    Masculino
+                  </label>
+                </div>
+                <div  className="form-check d-flex align-items-center gap-2 p-0">
+                  <input
+                    type="checkbox"
+                    className="ui-checkbox"
+                    value="Feminino"
+                    onChange={(e) => handleCheckbox(e, setGeneroSelecionado)}
+                  />
+                  <label className="form-check-label">
+                    Feminino
+                  </label>
+                </div>
+
+            </div>
+
+
+
+
+            <div className="mb-3">
+              <label>Gêneros Musicais</label>
+              {estilosMusicaisCheckbox.map((estilo) => (
+                <div key={estilo} className="form-check d-flex align-items-center gap-2 p-0">
+                  <input
+                    type="checkbox"
+                    className="ui-checkbox"
+                    value={estilo}  
+                    onChange={(e) => handleCheckbox(e, setEstilosSelecionados)}
+                  />
+                  <label className="form-check-label">
+                    { estilo.charAt(0).toUpperCase() + estilo.slice(1).toLowerCase()}
+                  </label>
+                </div>
+              ))}
+            </div>
+
+
+            <div className="mb-3">
+              <label>Instrumentos</label>
+              {instrumentosCheckBox.map((instrumento) => (
+                <div key={instrumento} className="form-check d-flex align-items-center gap-2 p-0">
+                  <input
+                    type="checkbox"
+                    className="ui-checkbox"
+                    value={instrumento}
+                    onChange={(e) => handleCheckbox(e, setInstrumentosSelecionados)}
+                  />
+                  <label className="form-check-label">
+                  { instrumento.charAt(0).toUpperCase() + instrumento.slice(1).toLowerCase()}
+                  </label>
+                </div>
+              ))}
+            </div>
+
+
+
+            
+     
+            <button className="btn-verperfil w-100 mt-2" onClick={filtragem}>
+              Filtrar
+            </button>
+          </div>
+        </div>
+
+
+        <div className="col-md-9">
+          <div className='container mb-5'>
+            <div className="row">
+              {usuariosFiltrados.length > 0 ? (
+                usuariosFiltrados.map(cards => (
                   <div className="col-md-4 mb-4" key={cards.id}>
                     <div className="custom-card text-center">
                       <div
                         className="top-section"
-                        style={{
-                          backgroundImage: `url(${cards.fundo})`,
-                        }}
+                        style={{ backgroundImage: `url(${cards.fundo})` }}
                       ></div>
 
                       <img src={cards.foto} alt="" className="profile-pic" />
@@ -135,18 +197,16 @@ export default function Users() {
                       <div className="icon-section mb-3"></div>
                     </div>
                   </div>
-                ))}
-              </div>
-
-              <div className='container-btn'>
-                <button className="btn-vermais" onClick={resposta}>
-                  Ver Mais
-                </button>
-              </div>
+                ))
+              ) : (
+                <div className="text-center w-100 mt-5">
+                  <h5>Nenhum usuário encontrado com os filtros selecionados.</h5>
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
