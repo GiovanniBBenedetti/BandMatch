c069bc8dc0bdfdbea52778aa81392b00e4c20858
DaviLeocadio <davi.leocadio.sen@gmail.com>
2025-04-25 13:04:46 -0300

    Iniciando página do user

--- src/app/user/[id]/page.jsx
+++ src/app/user/[id]/page.jsx
@@ -1,13 +1,27 @@
+import "./user.css";
+
 export default async function user({ params }) {
-    const userId = params.id;
+  const userId = params.id;
 
-    const user = await fetch("http://localhost:3333/users")
-        .then((response) => response.json())
+  const user = await fetch("http://localhost:3333/users").then((response) =>
+    response.json()
+  );
 
-    const userExibido = user[userId - 1];
+  const userExibido = user[userId - 1];
   return (
-  <>
-    {userExibido.nome}
-  </>
+    <>
+      <div className="">
+        <img
+          src={userExibido.fundo}
+          className="img-fluid background-img w-100"
+        />
+
+        <div className="rounded-circle align-items-center justify-content-center d-flex w-100">
+          <img src={userExibido.foto} className="rounded-circle perfil-img" />  
+          <div className="rounded-circle back-perfil"></div>
+        </div>
+        
+      </div>
+    </>
   );
 }
